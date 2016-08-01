b9a0e63d524fb775c229add6790e6c9a31314fff
interaktivarum <martin@interaktivarum.se>
2016-08-01 19:21:15 +0200

    Edited readme

--- rfid2server.py
+++ rfid2server.py
@@ -1,24 +1,3 @@
-# Example of detecting and reading a block from a MiFare NFC card.
-# Author: Tony DiCola
-# Copyright (c) 2015 Adafruit Industries
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 import binascii
 import sys
 
@@ -26,12 +5,6 @@ import Adafruit_PN532
 import requests, time, json
 
 read_sleep = 0.1;
-
-# Configuration for a Raspberry Pi:
-CS   = 18
-MOSI = 23
-MISO = 24
-SCLK = 25
 
 # Create an instance of the PN532 class.
 pn532 = Adafruit_PN532.PN532(cs=18, sclk=25, mosi=23, miso=24)
