ee3163b5d8103a9817c22089185e5ec1a63252d3
Aron Ambrosiani <aron@ambrosiani.se>
2016-12-19 12:38:05 +0100

    corrected typos taguid formatting settings.json placeholders

--- functions.py
+++ functions.py
@@ -2,23 +2,24 @@ import binascii, requests, json, datetime
 
 def sendRequest(ip,host,endpoint,static_user_data,uid,action):
 
-    data = {'userData':static_user_data, 'action':action, 'tagUid':binascii.hexlify(uid), 'time':datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')}
+    data = {'staticUserData':static_user_data, 'action':action, 'tagUid':binascii.hexlify(uid), 'time':datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')}
 
     print('\nSend server request to: ' + ip + " " + host)
 
     url = ''
-    headers = {'Content-Type': 'application/json', 'X-TagUID': uid}
+    headers = {'Content-Type': 'application/json', 'X-TagUID':binascii.hexlify(uid)}
     r = None
 
     if ip != '':
         url = 'http://' + ip + '/' + endpoint
-        headers = {'host': host, 'Content-Type': 'application/json', 'X-TagUID': uid}
+        headers = {'host': host, 'Content-Type': 'application/json', 'X-TagUID':binascii.hexlify(uid)}
     else:
         url = 'http://' + host + '/' + endpoint
 
     #Post request with host name
     try:
         r = requests.post(url, data=json.dumps(data), headers=headers, timeout=3)
+        print(r)
     except requests.exceptions.ConnectionError:
         print "Network connection error"
     except requests.exceptions.Timeout:
